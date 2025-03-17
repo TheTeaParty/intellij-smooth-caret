44fbd9d4f7f5387786713b2c313e0fdde3c969dd
nikita novikov <69369034+y9tr3ble@users.noreply.github.com>
2025-03-17 19:11:42 +0300

    added settings for blinking

--- src/main/kotlin/dev/gorokhov/smoothcaret/SmoothCaretSettings.kt
+++ src/main/kotlin/dev/gorokhov/smoothcaret/SmoothCaretSettings.kt
@@ -8,4 +8,6 @@ class SmoothCaretSettings {
     var caretWidth: Int = 2
     var caretStyle: CaretStyle = CaretStyle.LINE
     var caretColor: String = "CARET_COLOR"
+    var isBlinking: Boolean = true
+    var blinkInterval: Int = 500
 }
